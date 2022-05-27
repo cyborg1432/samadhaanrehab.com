f29c734f69b3d6a1d3f4a17edd056d388520dc7f
Deepu1250155 <33505884+Deepu1250155@users.noreply.github.com>
2022-05-27 20:13:53 +0530

    Final update done by deepanshu on 27-05-2022

--- dashboard/pdf.php
+++ dashboard/pdf.php
@@ -1,5 +1,21 @@
 <?php
 
-echo $_POST['base64File'];
+
+$heading = $_POST['heading'];
+$base64 = $_POST['base64File'];
+
+
+
+$conn = new mysqli('localhost','root','960ed4e74dcb54cd6363ff4b9bc8206fde0a280e96106803','rehab');
+
+$sql = "INSERT INTO pdffile (heading,base64) VALUES ('$heading', '$base64'); ";
+
+if ($conn->query($sql) === TRUE){
+	header('Location: http://samadhaanrehab.com/dashboard/create-post.html?value=sent');
+}else{
+        header('Location: http://samadhaanrehab.com/dashboard/create-post.html?value=notsent');
+
+}
+
 
 ?>
